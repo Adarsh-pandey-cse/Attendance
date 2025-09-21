02097f9b2f3ad42b121a93323e9f627fed3d53e4
Adarsh Pandey <adarshpandey5476@gmail.com>
2025-09-21 12:01:22 +0000

    remove marquee concept and aligh left side vertical beads in side

--- src/components/radha-rani-theme-elements.tsx
+++ src/components/radha-rani-theme-elements.tsx
@@ -39,27 +39,6 @@ export function RadhaRaniThemeElements() {
           </div>
       </div>
 
-      <div className="fixed bottom-0 left-0 w-full h-10 overflow-hidden z-50 pointer-events-none yellow-stripped-background py-1">
-        <motion.div
-          className="absolute whitespace-nowrap"
-          initial={{ x: '100%' }}
-          animate={{ x: '-100%' }}
-          transition={{
-            duration: 20,
-            repeat: Infinity,
-            ease: 'linear'
-          }}
-        >
-          <p className="font-hindi text-2xl font-bold bg-gradient-to-r from-pink-500 to-black bg-clip-text text-transparent" style={{ fontFamily: "'Tiro Devanagari Hindi', serif" }}>
-            <span className='mx-8'>राधा राधा</span>
-            <span className='mx-8'>राधा राधा</span>
-            <span className='mx-8'>राधा राधा</span>
-            <span className='mx-8'>राधा राधा</span>
-            <span className='mx-8'>राधा राधा</span>
-            <span className='mx-8'>राधा राधा</span>
-          </p>
-        </motion.div>
-      </div>
     </>
   );
 }

9d7274cb1b4c84f2a97902995c0fd8df7d8f1b9b
Adarsh Pandey <adarshpandey5476@gmail.com>
2025-09-21 10:25:28 +0000

    resize the border of radha rani image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ const DivineImage = () => {
   return (
     <div className="relative group my-6 flex justify-center">
        <div 
-        className="absolute -inset-1.5 rounded-full bg-gradient-to-r from-yellow-400 via-red-500 to-orange-500 opacity-75 blur-lg transition duration-1000 group-hover:opacity-100 group-hover:duration-200 animate-tilt"
+        className="absolute -inset-1 rounded-full bg-gradient-to-r from-yellow-400 via-red-500 to-orange-500 opacity-75 blur-lg transition duration-1000 group-hover:opacity-100 group-hover:duration-200 animate-tilt"
       ></div>
       <Image
         src={image.imageUrl}

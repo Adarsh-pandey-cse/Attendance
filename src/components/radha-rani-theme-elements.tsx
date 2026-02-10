74e9329fb2c4e2620f77beb7f6f72824be09ddd4
Adarsh Pandey <adarshpandey5476@gmail.com>
2026-02-10 18:38:14 +0000

    remove the concept of radha naam beads from the radha rani theme

--- src/components/radha-rani-theme-elements.tsx
+++ src/components/radha-rani-theme-elements.tsx
@@ -1,9 +1,7 @@
-
 'use client';
 
 import { useTheme } from '@/hooks/use-theme';
 import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
 
 export function RadhaRaniThemeElements() {
   const { theme } = useTheme();
@@ -17,28 +15,9 @@ export function RadhaRaniThemeElements() {
     return null;
   }
 
-  const beadContent = Array(50).fill('राधा').map((text, index) => (
-    <span key={index} className="bead">{text}</span>
-  ));
-
   return (
     <>
       <div className="radha-rani-background"></div>
-
-      {/* Left and Right Bead Columns */}
-      <div className="bead-container left-beads">
-          <div className="bead-column-down">
-              {beadContent}
-              {beadContent}
-          </div>
-      </div>
-       <div className="bead-container right-beads">
-          <div className="bead-column-up">
-              {beadContent}
-              {beadContent}
-          </div>
-      </div>
-
     </>
   );
 }
